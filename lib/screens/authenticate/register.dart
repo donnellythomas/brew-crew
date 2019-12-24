682f17f25d6a0ad46041b4392b4f150ee982bf1b
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-12-24 11:20:39 -0500

    sign in and register forms with toggle button

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,20 +1,18 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   final Function toggleView;
-  SignIn({this.toggleView});
+  Register({this.toggleView});
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
-
   //text field state
   String email = '';
   String password = '';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +20,13 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+        title: Text('Sign up to Brew Crew'),
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(
               Icons.person,
             ),
-            label: Text('Register'),
+            label: Text('Sign in'),
             onPressed: () {
               widget.toggleView();
             },
@@ -63,7 +61,7 @@ class _SignInState extends State<SignIn> {
                 RaisedButton(
                   color: Colors.pink[400],
                   child: Text(
-                    'Sign in',
+                    'Register',
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () async {
