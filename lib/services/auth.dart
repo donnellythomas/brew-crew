1455cb812e10e7f1fa0ecfdab24d17075e87ce8f
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-12-24 10:08:08 -0500

    added custom user class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user odj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   //sign in anonomously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
